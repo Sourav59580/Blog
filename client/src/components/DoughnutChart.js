33aa37519fa97e3813c1f9b6e570009d5d3f1467
sourav santra <souravsantra59580@gmail.com>
2020-04-14 12:00:49 +0530

    eight commit(13/04/2020)

--- client/src/components/DoughnutChart.js
+++ client/src/components/DoughnutChart.js
@@ -3,13 +3,13 @@ import { Doughnut } from 'react-chartjs-2'
 
 function DoughnutChart() {
    const data = {
-       labels : ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+       labels : ['Jan','Feb','Mar','Apr'],
        datasets : [
            {
                label : 'Contribution activity for 2020',
                data : [3,10,4,8],
-               borderColor : ['#ff8c94'],
-               backgroundColor : ['pink'],
+               borderColor : ['#f4e04d','#00bcd4','#ff8c94','#21bf73'],
+               backgroundColor : ['#f2ed6f','#b2ebf2','pink','#baf1a1'],
                pointBackgroundColor : ['pink'],
                pointBorderColor : ['#ff8c94']
                
